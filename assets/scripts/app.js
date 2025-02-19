a68c5bbec5feac802dc9ec8420b569a27f12f303
Cyber Lord <braxtonnzenong@gmail.com>
2025-02-19 17:57:25 +0100

    Nav bar made responsive but faced errors somewhat after over doing things

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,25 @@
+
+// Giving the current year to the footer in real-time
 const footerYear = document.querySelector('.year');
 footerYear.textContent = new Date().getFullYear();
+
+// Navbar toggle
+const profileTrigger = document.querySelector('nav .tail-tablet button');
+const profileMenu = profileTrigger.nextElementSibling;
+
+console.log(profileMenu);
+
+profileTrigger.addEventListener('click', () => {
+  profileMenu.style.display = 'block';
+  profileTrigger.style.display = 'none';
+
+  console.log(profileMenu);
+});
+
+document.body.addEventListener('click', (e) => {
+  if (e.target == profileTrigger) {
+    profileMenu.style.display = 'none';
+    profileMenu.style.width.value = '0';
+    profileTrigger.style.display = 'block';
+  }
+});
